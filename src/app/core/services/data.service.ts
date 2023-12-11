ca0e7e4238815145c4ac28b69a9e34f13cbfbb7c
Yurii <zumbala2013@gmail.com>
2023-12-11 21:12:12 +0200

    Refactor fetch methods

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -6,13 +6,36 @@ import { Observable } from "rxjs";
     providedIn: "root",
 })
 export class DataService {
+    private CATEGORY_URL = "http://localhost:3000/categories";
+    private DISH_URL = "http://localhost:3000/dishes";
+
     constructor(private http: HttpClient) {}
 
     addCategory(data: any): Observable<any> {
-        return this.http.post("http://localhost:3000/categories", data);
+        return this.http.post(this.CATEGORY_URL, data);
+    }
+
+    getCategoryById(id: string): Observable<any> {
+        return this.http.get(this.CATEGORY_URL + id);
     }
 
     getCategories(): Observable<any> {
-        return this.http.get("http://localhost:3000/categories");
+        return this.http.get(this.CATEGORY_URL);
+    }
+
+    addDish(data: any): Observable<any> {
+        return this.http.post(this.DISH_URL, data);
+    }
+
+    getDishById(id: string): Observable<any> {
+        return this.http.get(this.DISH_URL + id);
+    }
+
+    getDishesByCategoryId(id: string) {
+        return this.http.get(this.CATEGORY_URL + id + "dishes");
+    }
+
+    getDishes(): Observable<any> {
+        return this.http.get(this.DISH_URL);
     }
 }
