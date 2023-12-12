248d51766a7f9687c65fa3cd395ee0c80ac127c7
Yurii <zumbala2013@gmail.com>
2023-12-12 16:30:20 +0200

    Reconfigure modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { AppComponent } from "./components/app/app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "./shared/material.module";
 import { MenuModule } from "./menu/menu.module";
 import { SharedModule } from "./shared/shared.module";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-import { DataService } from "./services/data.service";
+import { DataService } from "./shared/services/data.service";
 import { RouterLink } from "@angular/router";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
--- src/app/menu/components/dishes/dishes.component.ts
+++ src/app/menu/components/dishes/dishes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from 'src/app/services/data.service';
+import { DataService } from 'src/app/shared/services/data.service';
 import { Dish } from 'src/app/shared/classes/Dish';
 
 @Component({
