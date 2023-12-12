0818d8a6e6372b358e20ac6f6279216429e7a95c
Yurii <zumbala2013@gmail.com>
2023-12-12 15:56:35 +0200

    Fix routerLink error

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./components/header/header.component";
 import { FooterComponent } from "./components/footer/footer.component";
 import { MaterialModule } from "../shared/material.module";
+import { RouterLink } from "@angular/router";
 
 @NgModule({
   declarations: [HeaderComponent, FooterComponent],
-  imports: [CommonModule, MaterialModule],
+  imports: [CommonModule, MaterialModule, RouterLink],
   exports: [HeaderComponent, FooterComponent],
 })
 export class CoreModule { }
--- src/app/menu/menu.module.ts
+++ src/app/menu/menu.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MenuRoutingModule } from "./menu-routing.module";
-import { MainComponent } from "./components/main/main.component";
 import { CategoriesComponent } from './components/categories/categories.component';
 import { DishesComponent } from './components/dishes/dishes.component';
 import { WelcomeComponent } from './pages/welcome/welcome/welcome.component';
 import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
-    declarations: [MainComponent, CategoriesComponent, DishesComponent, WelcomeComponent, MenuComponent],
+    declarations: [CategoriesComponent, DishesComponent, WelcomeComponent, MenuComponent],
     imports: [CommonModule, MenuRoutingModule],
-    exports: [MainComponent, MenuComponent, CategoriesComponent, DishesComponent, WelcomeComponent],
+    exports: [MenuComponent, CategoriesComponent, DishesComponent, WelcomeComponent],
 })
 export class MenuModule {}
